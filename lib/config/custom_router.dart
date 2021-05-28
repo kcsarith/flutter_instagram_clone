29fe02497c7e4283fdcf81892c670e14c2fef117
kcsarith <krisnasarith@gmail.com>
2021-05-28 12:02:44 -0700

    Create custom_router.dart

---
+++ lib/config/custom_router.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/screens/screens.dart';
+
+class CustomRouter {
+  static Route onGenerateRoute(RouteSettings settings) {
+    print('Route: ${settings.name}');
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: '/'),
+          builder: (_) {
+            return const Scaffold();
+          },
+        );
+      case SplashScreen.routeName:
+        return SplashScreen.route();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route _errorRoute() {
+    return MaterialPageRoute(
+        settings: const RouteSettings(name: '/error'),
+        builder: (_) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Error'),
+            ),
+            body: const Center(
+              child: Text('Something went wrong!'),
+            ),
+          );
+        });
+  }
+}
