e49f4641caab96bdc0d3103b89ba8d3baa55c625
kcsarith <krisnasarith@gmail.com>
2021-07-18 06:29:08 -0700

    add BlocListener to Scaffold

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_instagram_clone/blocs/blocs.dart';
+
+import '../screens.dart';
 
 class SplashScreen extends StatelessWidget {
   static const String routeName = '/splash';
@@ -15,9 +19,18 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
+      child: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state.status == AuthStatus.unauthenticated) {
+            Navigator.of(context).pushNamed(LoginScreen.routeName);
+          } else if (state.status == AuthStatus.authenticated) {
+            Navigator.of(context).pushNamed(NavScreen.routeName);
+          }
+        },
+        child: Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
         ),
       ),
     );
