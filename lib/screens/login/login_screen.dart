ab1d559b819c78709dc6f45bdb6f93f4de751d1a
kcsarith <krisnasarith@gmail.com>
2021-07-21 17:35:29 +0900

    add BlocProvider and Cosumer to LoginScreen.

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_instagram_clone/repositories/repositories.dart';
+import 'package:flutter_instagram_clone/screens/login/cubit/login_cubit.dart';
+import 'package:flutter_instagram_clone/widgets/widgets.dart';
+
+import '../screens.dart';
 
 class LoginScreen extends StatelessWidget {
   static const String routeName = '/login';
@@ -9,7 +15,10 @@ class LoginScreen extends StatelessWidget {
         transitionDuration: const Duration(seconds: 0),
         pageBuilder: (BuildContext context, Animation<double> animDouble1,
             Animation<double> animDouble2) {
-          return LoginScreen();
+          return BlocProvider<LoginCubit>(
+              create: (_) =>
+                  LoginCubit(authRepository: context.read<AuthRepository>()),
+              child: LoginScreen());
         });
   }
 
@@ -22,85 +31,115 @@ class LoginScreen extends StatelessWidget {
       },
       child: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: Center(
-              child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Card(
-                color: Colors.white,
+        child: BlocConsumer<LoginCubit, LoginState>(
+          listener: (context, state) {
+            if (state.status == LoginStatus.error) {
+              showDialog(
+                context: context,
+                builder: (context) =>
+                    ErrorDialog(content: state.failure.message),
+              );
+            }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: Center(
                 child: Padding(
                   padding: const EdgeInsets.all(24.0),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(
-                          'Goztagram',
-                          style: TextStyle(
-                            fontSize: 28.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
+                  child: Card(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              'Goztagram',
+                              style: TextStyle(
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 12.0),
+                            TextFormField(
+                              decoration: InputDecoration(hintText: 'email'),
+                              onChanged: (value) {
+                                return context
+                                    .read<LoginCubit>()
+                                    .emailChanged(value);
+                              },
+                              validator: (value) {
+                                return !value.contains('@')
+                                    ? 'Please enter a valid email.'
+                                    : null;
+                              },
+                            ),
+                            const SizedBox(height: 16.0),
+                            TextFormField(
+                                decoration: InputDecoration(hintText: 'password'),
+                              obscureText: true,
+                              onChanged: (value) {
+                                return context
+                                    .read<LoginCubit>()
+                                    .passwordChanged(value);
+                              },
+                              validator: (value) {
+                                return value.length < 6
+                                    ? 'Must be at least 6 characters.'
+                                    : null;
+                              },
+                            ),
+                            const SizedBox(height: 28.0),
+                            ElevatedButton(
+                              onPressed: () => _submitForm(
+                                context,
+                                state.status == LoginStatus.submitting,
+                              ),
+                              child: Text(
+                                'Log in',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                primary: Theme.of(context).primaryColor,
+                                elevation: 1.0,
+                              ),
+                            ),
+                            const SizedBox(height: 12.0),
+                            ElevatedButton(
+                              onPressed: () => Navigator.of(context)
+                                  .pushNamed(SignupScreen.routeName),
+                              child: Text(
+                                'No account? Sign Up.',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.grey,
+                                elevation: 1.0,
+                              ),
+                            ),
+                          ],
                         ),
-                        const SizedBox(height: 12.0),
-                        TextFormField(
-                          decoration: InputDecoration(hintText: 'email'),
-                          onChanged: (value) {
-                            return print(value);
-                          },
-                          validator: (value) {
-                            return !value.contains('@')
-                                ? 'Please enter a valid email.'
-                                : null;
-                          },
-                        ),
-                        const SizedBox(height: 16.0),
-                        TextFormField(
-                          decoration: InputDecoration(hintText: 'password'),
-                          onChanged: (value) {
-                            return print(value);
-                          },
-                          validator: (value) {
-                            return value.length < 6
-                                ? 'Must be at least 6 characters.'
-                                : null;
-                          },
-                        ),
-                        const SizedBox(height: 28.0),
-                        ElevatedButton(
-                          onPressed: () => print('Login'),
-                          child: Text(
-                            'Submit',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Theme.of(context).primaryColor,
-                            elevation: 1.0,
-                          ),
-                        ),
-                        const SizedBox(height: 12.0),
-                        ElevatedButton(
-                          onPressed: () => print('Navigate to signup screen'),
-                          child: Text(
-                            'No account? Sign up.',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.grey,
-                            elevation: 1.0,
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                      ),
                   ),
-                )),
-          )),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
+  }
+
+  void _submitForm(BuildContext context, bool isSubmitting) {
+    if (_formKey.currentState.validate() && !isSubmitting) {
+      context.read<LoginCubit>().logInWithCredentials();
+    }
   }
 }
