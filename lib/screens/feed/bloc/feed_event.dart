8fc934a476767cbb509aa90b41e6dac2f3bb091a
kcsarith <krisnasarith@gmail.com>
2021-08-13 19:39:41 -0700

    implement feed

---
+++ lib/screens/feed/bloc/feed_event.dart
@@ -0,0 +1,13 @@
+part of 'feed_bloc.dart';
+
+abstract class FeedEvent extends Equatable {
+  const FeedEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeedFetchPosts extends FeedEvent {}
+
+class FeedPaginatePosts extends FeedEvent {}
+
