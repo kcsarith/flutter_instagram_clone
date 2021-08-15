aac6a52eec0a34e031814e0597f7bf960256f1b3
kcsarith <krisnasarith@gmail.com>
2021-08-14 19:20:03 -0700

    add notifications

--- lib/screens/notifications/notifications_screen.dart
+++ lib/screens/notifications/notifications_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_instagram_clone/widgets/widgets.dart';
+
+import 'bloc/notifications_bloc.dart';
+import 'widgets/widgets.dart';
 
 class NotificationsScreen extends StatelessWidget {
   static const String routeName = '/notifications';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Notifications'),
+      appBar: AppBar(title: const Text('Notifications')),
+      body: BlocBuilder<NotificationsBloc, NotificationsState>(
+        builder: (context, state) {
+          switch (state.status) {
+            case NotificationsStatus.error:
+              return CenteredText(text: state.failure.message);
+            case NotificationsStatus.loaded:
+              return ListView.builder(
+                itemCount: state.notifications.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final notification = state.notifications[index];
+                  return NotificationTile(notification: notification);
+                },
+              );
+            default:
+              return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
